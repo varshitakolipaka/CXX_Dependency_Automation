eaa49ea2f8d53296cb0ba62df779a8e1bcc488ad
Gaurav Singh <gauravsinghk03@gmail.com>
2021-10-07 17:10:03 +0530

    Added functionality to add the file structure.

--- better.py
+++ better.py
@@ -1,5 +1,6 @@
 import os
-
+import shutil
+from pathlib import Path
 import re
 # input_path = str(input('input file filename: '))
 input_path = "opencv2/core.hpp"
@@ -49,13 +50,13 @@ included_libraries = {}
 
 while len(include_list) > 0:
     if include_list[0] not in included_libraries:
-        # print("CURRENT PARENT:", include_list[0])
         included_libraries[include_list[0]] = []
         get_all_dependencies(include_list[0])
     include_list.pop(0)
 
 # path_to_remove = str(input("Enter the path you want to remove: "))
-path_to_remove = "/Users/vars/OneDrive - International Institute of Information Technology/CXX_Dependency_Automation/opencv2/"
+path_to_remove = os.getcwd()
+print(path_to_remove)
 graphviz_code = "strict digraph G{\n"
 for parent, children in included_libraries.items():
     clean_parent = parent.replace(path_to_remove,'')
@@ -65,3 +66,22 @@ for parent, children in included_libraries.items():
     
 graphviz_code = graphviz_code + "\n}"
 print(graphviz_code)
+
+desired_path = str(input("Enter the desired directory, where you want your root file to be saved: "))
+#create desired file if not created 
+# if not os.path.exists(desired_path):
+#     os.makedirs(desired_path)
+# print(included_libraries)
+
+for file_path in included_libraries.items():
+    exact_file_path = file_path[0].replace(path_to_remove, desired_path)
+    # print(exact_file_path)
+    relative_file_path = file_path[0].replace(path_to_remove, "")
+    exact_directory_path = re.match(r'^(.*[\/])', exact_file_path).group(1)
+    print(exact_directory_path)
+    # os.makedirs(exact_directory_path,exist_ok=True)
+    # # os.makedirs(exact_directory_path)
+    os.makedirs(exact_directory_path, mode = 0o777, exist_ok=True)
+    if(os.path.exists(file_path[0])):
+        open(exact_file_path, 'w').close()
+        shutil.copy(file_path[0], exact_file_path)
