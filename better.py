08e89fb7e23b468f29fd42e619736ac4b42461d5
varshitakolipaka <varshita2003@gmail.com>
2021-10-11 20:26:44 +0400

    Fixed minor bugs

--- better.py
+++ better.py
@@ -2,31 +2,28 @@ import os
 import shutil
 from pathlib import Path
 import re
-# input_path = str(input('input file filename: '))
-input_path = "opencv2/core.hpp"
-include_list = []
-input_path = os.getcwd() + '/' + input_path
 
+def getFolderPath(file_path):
+    folder_path = re.match("^(.*[\/])", file_path).group(1)
+    return folder_path
 
-def get_all_dependencies(file_path):
-
-    # get path of the file
-    new_dir = re.match(r'^(.*[\/])', file_path).group(1)
-    # print("new dir is:", new_dir)
+def getIncludes(myline):
+    match = re.findall('(#include "(.*?.(h|hpp))")', myline)
+    include_file_name = match[0][1] if len(match) else "INVALID"
+    return include_file_name
+    
+def getDependencies(file_path):
+    path_to_file = getFolderPath(file_path)
     if os.path.exists(file_path):
         myfile = open(file_path, "r")
-        # print("FILE: ", file_path)
         myline = myfile.readline()
         while myline:
-            list1 = (re.findall('(#include \"(.*?.(h|hpp))\")', myline))
-            # print(len(list1))
-            if(len(list1) == 1):
-                actual = list1[0][1]
-                new_file_path = optimize_file_path(actual, new_dir)
-                include_list.append(new_file_path)  # append to global list
-                included_libraries[include_list[0]].append(new_file_path)
+            include_file_name = getIncludes(myline)
+            if include_file_name != "INVALID":
+                shortened_file_path = compressFilePath(include_file_name, path_to_file)
+                include_list.append(shortened_file_path)
+                included_libraries[include_list[0]].append(shortened_file_path)
 
-                # print(new_dir + actual)
             myline = myfile.readline()
 
         myfile.close()
@@ -34,56 +31,112 @@ def get_all_dependencies(file_path):
     return include_list
 
 
-def optimize_file_path(file_path, new_dir):
-    actual = file_path
-    while(actual.startswith('./')):
-        actual = actual[2:]
-        # print("actual:", actual)
-    while (actual.startswith('../')):
-        new_dir = re.match('^(.*[\/])', new_dir).group(1)
-        actual = actual[3:]
-    return new_dir+actual
 
+def compressFilePath(file_path, folder_path):
+
+    while file_path.startswith("./"):
+        file_path = file_path[2:] 
+    
+    while file_path.startswith( "../"):
+        print("CAME HEREEE!!!!!    ", file_path)
+        print("BEFORE: ", folder_path)
+        if folder_path.endswith("/"):
+            folder_path = folder_path[:-1]
+        folder_path = re.match("^(.*[\/])", folder_path).group(1)
+        print("AFTER: ", folder_path)
+        file_path = file_path[3:]
+    # print(folder_path + file_path)
+    return folder_path + file_path
+
+def find_common_path(dictionary):
+    array = keys_to_arr(dictionary)
+    ind1 = 0
+    ind2 = 1
+    common = ""
+    while(ind1 < len(array) and ind2 < len(array)):
+        common = os.path.commonpath([array[ind1], array[ind2]])
+        ind1+=1
+        ind2+=1
+    if not common.endswith("/"):
+        common = re.match("^(.*[\/])", common).group(1)
+
+    return common
+
+def rename_keys_dict(common_path, required_path, dictionary):
+    for key in dictionary:
+        new_key = key.replace(common_path, required_path)
+        print("new_key: ",new_key)
+        dictionary[new_key] = dictionary.pop(key)
+        return dictionary
+    
+def keys_to_arr(dictionary):
+    arr = []
+    for key in dictionary:
+        arr.append(key)
+    return arr
+def processIntactPath(intact_path):
+    if not intact_path.startswith("/"):
+        intact_path = "/" + intact_path
+    if not intact_path.endswith("/"):
+        intact_path = intact_path[:-1]
+    return intact_path
+# --------works----------
+
+# main
+
+input_path = input("File Path: ")
+
+
+include_list = []
+if input_path.startswith("/"):
+    input_path = os.path.abspath(input_path)
 
 include_list.append(input_path)
 included_libraries = {}
 
+
 while len(include_list) > 0:
     if include_list[0] not in included_libraries:
         included_libraries[include_list[0]] = []
-        get_all_dependencies(include_list[0])
+        getDependencies(include_list[0])
     include_list.pop(0)
 
-# path_to_remove = str(input("Enter the path you want to remove: "))
-path_to_remove = os.getcwd()
-print(path_to_remove)
-graphviz_code = "strict digraph G{\n"
+# --------idknow-----------
+temp_paths = []
+
+intact_path = input("Enter the directory structure to keep intact (Must be a substring from the end): ")
+intact_path = processIntactPath(intact_path)
+
+path_to_remove = find_common_path(included_libraries) 
+graphviz_code = "strict digraph G {\n"
 for parent, children in included_libraries.items():
-    clean_parent = parent.replace(path_to_remove, '')
+    clean_parent = parent.replace(path_to_remove, "")
     graphviz_code = graphviz_code + '"' + clean_parent + '"' + "-> { "
-    s = '; '.join(str('"' + child.replace(path_to_remove, '') + '"')
-                  for child in children)
-    graphviz_code = graphviz_code + s + '};\n'
+    s = "; ".join(
+        str('"' + child.replace(path_to_remove, "") + '"') for child in children
+    )
+    graphviz_code = graphviz_code + s + "};\n"
 
 graphviz_code = graphviz_code + "\n}"
 print(graphviz_code)
 
-desired_path = str(input(
-    "Enter the desired directory, where you want your root file to be saved: "))
-# create desired file if not created
-# if not os.path.exists(desired_path):
-#     os.makedirs(desired_path)
-# print(included_libraries)
+desired_path = str(
+    input("Enter the desired directory, where you want your root file: ")
+)
+desired_path += intact_path
+if not desired_path.endswith("/"):
+    desired_path += "/" 
 
 for file_path in included_libraries.items():
+    # print("FILE PATH: ",file_path)
+    # print("file path; ", file_path[0])
+    # print("REMOVED THIS: ", path_to_remove)
     exact_file_path = file_path[0].replace(path_to_remove, desired_path)
-    # print(exact_file_path)
-    relative_file_path = file_path[0].replace(path_to_remove, "")
-    exact_directory_path = re.match(r'^(.*[\/])', exact_file_path).group(1)
-    print(exact_directory_path)
-    # os.makedirs(exact_directory_path,exist_ok=True)
-    # # os.makedirs(exact_directory_path)
+    # print("exact file path; ",exact_file_path)
+    exact_directory_path = re.match(r"^(.*[\/])", exact_file_path).group(1)
+    # print(exact_directory_path)
+    # print("exact directory path; ",exact_directory_path)
     os.makedirs(exact_directory_path, mode=0o777, exist_ok=True)
-    if(os.path.exists(file_path[0])):
-        open(exact_file_path, 'w').close()
+    if os.path.exists(file_path[0]):
+        open(exact_file_path, "w").close()
         shutil.copy(file_path[0], exact_file_path)
