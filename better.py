b23d615e969e0cba9e418ee2333d17f9aa241600
varshitakolipaka <varshita2003@gmail.com>
2021-11-19 23:22:04 +0400

    another one bites the dust

--- better.py
+++ better.py
@@ -6,25 +6,54 @@ import pydot
 
 
 def getFolderPath(file_path):
+    # get everything before the last slash
     folder_path = re.match("^(.*[\/])", file_path).group(1)
     return folder_path
 
 def getIncludes(myline):
-    match = re.findall('(#include "(.*?.(h|hpp))")', myline)
-    include_file_name = match[0][1] if len(match) else "INVALID"
+    # finds all instances of include patterns in a line, should find only 1
+    # (#include *\"(.*?.(h|hpp))\")|
+    match = re.findall('(#include *\"(.*?.(h|hpp))\")|(#include *\<(.*?.(h|hpp))\>)', myline)
+    # checks in [()], and whether such an array exists, else, an include is not found
+    if len(match):
+        print(match)
+        if match[0][1] != "":
+            include_file_name = match[0][1]
+        elif match[0][4] and match[0][4] != "":
+            include_file_name = match[0][4]
+    else:
+        include_file_name = "INVALID" 
+    
     return include_file_name
     
-def getDependencies(file_path):
+def getDependencies(file_path, included_libraries):
+    print("----------------------")
+    print("INSIDE:", file_path)
+    print("----------------------")
+    # gets only the folder path
     path_to_file = getFolderPath(file_path)
-    if os.path.exists(file_path):
+    # check if such a file exists, and it isn't empty stringed
+    if os.path.exists(file_path) and file_path != "" and os.path.isdir(file_path) != 1:
+        # opening the file
         myfile = open(file_path, "r")
         myline = myfile.readline()
+        # do for each line in the file, while reading line by line
         while myline:
+            # get an instance of #include
             include_file_name = getIncludes(myline)
+            # if something is being included...
             if include_file_name != "INVALID":
-                shortened_file_path = compressFilePath(include_file_name, path_to_file)
-                include_list.append(shortened_file_path)
-                included_libraries[include_list[0]].append(shortened_file_path)
+                
+                # if file not found, try finding file in the root directory of file
+                print("Raw include:", include_file_name)
+                include_file_name = compressFilePath(include_file_name, path_to_file)
+                include_file_name = processInputPath(path_to_file, include_file_name)
+
+                # print("NEW PATH: ", include_file_name)
+                
+                print("Including: ", include_file_name)
+                include_list.append(include_file_name)
+                included_libraries[include_list[0]].append(include_file_name)
 
             myline = myfile.readline()
 
@@ -33,36 +62,31 @@ def getDependencies(file_path):
     return include_list
 
 def compressFilePath(file_path, folder_path):
-
+    if file_path.startswith("/"):
+        return file_path
     while file_path.startswith("./"):
-        file_path = file_path[2:] 
+        file_path = file_path[2:]
     
     while file_path.startswith( "../"):
         if folder_path.endswith("/"):
             folder_path = folder_path[:-1]
         folder_path = re.match("^(.*[\/])", folder_path).group(1)
         file_path = file_path[3:]
-    print(folder_path + file_path)
+    # print(folder_path + file_path)
     return folder_path + file_path
 
 def find_common_path(dictionary):
     array = keys_to_arr(dictionary)
-    ind1 = 0
-    ind2 = 1
-    common = ""
-    while(ind1 < len(array) and ind2 < len(array)):
-        common = os.path.commonpath([array[ind1], array[ind2]])
-        ind1+=1
-        ind2+=1
-    if not common.endswith("/"):
-        common = re.match("^(.*[\/])", common).group(1)
+    common_path = os.path.commonpath(array)
+    return common_path
+    # if not common.endswith("/"):
+    #     common = re.match("^(.*[\/])", common).group(1)
 
-    return common
 
 def rename_keys_dict(common_path, required_path, dictionary):
     for key in dictionary:
         new_key = key.replace(common_path, required_path)
-        print("new_key: ",new_key)
+        # print("new_key: ",new_key)
         dictionary[new_key] = dictionary.pop(key)
         return dictionary
     
@@ -89,33 +113,51 @@ def createDotFile(included_libraries, path_to_remove):
         graphviz_code = graphviz_code + s + "};\n"
 
     graphviz_code = graphviz_code + "\n}"
-    print(graphviz_code)
+    # print(graphviz_code)
     graph, = pydot.graph_from_dot_data(graphviz_code)
     image_path = input("Enter Image Path:")
     graph.write_png(image_path )
 
-def initIncludeDict(included_libraries):
+def initIncludeDict(included_libraries, include_list):
     while len(include_list) > 0:
         if include_list[0] not in included_libraries:
             included_libraries[include_list[0]] = []
-            getDependencies(include_list[0])
+            getDependencies(include_list[0], included_libraries)
         include_list.pop(0)
 
+def processInputPath(folder_path,input_path):
+    path = folder_path + input_path
+    
+    if input_path.startswith("/"):
+        path = input_path
+    if not os.path.exists(folder_path+input_path) or os.path == "":
+        temp_path = "/Users/vars/OneDrive - International Institute of Information Technology/CXX_Dependency_Automation/" + input_path
+        if os.path.exists(temp_path):
+            path = temp_path
+        path = os.path.abspath(path)
+    return path
+
+
+
+# Driver code ------------------------------------
+
+# unfiltered, every include is added here
 include_list = []
+# final includes, with no repeats are added here
 included_libraries = {}
 
-input_path = input("File Path: ")
 
-if input_path.startswith("/"):
-    input_path = os.path.abspath(input_path)
+input_path = input("File Path: ")
+input_path = processInputPath("", input_path)
+
 
 include_list.append(input_path)
-initIncludeDict(included_libraries)
+initIncludeDict(included_libraries, include_list)
 path_to_remove = find_common_path(included_libraries)
 
 createDotFile(included_libraries, path_to_remove)
 
-intact_path = input("Enter the directory structure to keep intact (Must be a substring from the end): ")
+intact_path = input("Enter the directory structure to keep intact (Must be a substring of original path from the end): ")
 intact_path = processIntactPath(intact_path)
 
 desired_path = str(
@@ -130,5 +172,9 @@ for file_path in included_libraries.items():
     exact_directory_path = re.match(r"^(.*[\/])", exact_file_path).group(1)
     if os.path.exists(file_path[0]):
         os.makedirs(exact_directory_path, mode=0o777, exist_ok=True)
-        open(exact_file_path, "w").close()
-        shutil.copy(file_path[0], exact_file_path)
+        if os.path.isdir(exact_file_path) != 1:
+            open(exact_file_path, "w").close()
+            # check if file_path[0] and exact_file_path aren't the same file
+            if file_path[0] != exact_file_path:
+                print("Copying: ", file_path[0], " to ", exact_file_path)
+                shutil.copy(file_path[0], exact_file_path)
